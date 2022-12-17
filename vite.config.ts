9b9c6247cec61fa6d7e671ce00350e040106db60
unknown <asitsingh0@gmail.com>
2022-12-17 20:43:28 +0530

    adding dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/alpha-mans-fashion/',
+  base: 'https://asitsi.gitlab.io/alpha-mans-fashion/',
   plugins: [react()],
 })
